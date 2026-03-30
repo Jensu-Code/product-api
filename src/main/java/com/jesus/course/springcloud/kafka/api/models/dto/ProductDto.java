4e7c16dcf985820da89b30f2b85ee31463fd7f5c
Jesús Emanuel <106935975+Jensu-Code@users.noreply.github.com>
2026-03-30 17:26:21 -0500

    add product-api

---
+++ src/main/java/com/jesus/course/springcloud/kafka/api/models/dto/ProductDto.java
@@ -0,0 +1,9 @@
+package com.jesus.course.springcloud.kafka.api.models.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductDto(@NotBlank String name, @NotNull @Min(10) Double price) {
+
+}
---
+++ src/test/java/com/jesus/course/springcloud/kafka/api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.course.springcloud.kafka.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
