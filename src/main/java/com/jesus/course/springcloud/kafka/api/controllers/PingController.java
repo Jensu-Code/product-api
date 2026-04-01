0aaed42cc185d9bd86d5c8ee96ecb61fe9aa4f2f
Jesús Emanuel <106935975+Jensu-Code@users.noreply.github.com>
2026-04-01 18:20:27 -0500

    add controller pruena

---
+++ src/main/java/com/jesus/course/springcloud/kafka/api/controllers/PingController.java
@@ -0,0 +1,13 @@
+package com.jesus.course.springcloud.kafka.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+   
+    @GetMapping("/ping")
+    public String ping() {
+        return "ping product api";
+    }
+}
